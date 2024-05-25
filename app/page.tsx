4aae71ea7fe7867ac1ac144849f6e844afea583d
Bogdan Vol <bogdan.r.volintiru@gmail.com>
2024-05-25 20:00:29 +0300

    * list and filters page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function Pokedex() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>App Router</h1>
+    <main className="p-2">
+      <h1 className="text-5xl">Pokedex</h1>
+      <section aria-label="filters" title="Filters"></section>
+      <section aria-label="list" title="List"></section>
     </main>
   );
 }
